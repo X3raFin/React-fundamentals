7d520e99912d42908359bf778b1c094d55c8dace
Kacper <kapi98981@gmail.com>
2025-07-04 19:22:03 +0200

    Added counter to cart in nav

--- ShoopList/src/pages/NavPage.jsx
+++ ShoopList/src/pages/NavPage.jsx
@@ -1,11 +1,19 @@
 import "../App.css"
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useCart } from "./CartContext";
+import { useEffect } from "react";
 
 function NavPage() {
+	const {getQuantity, cartStatus} = useCart();
+
 	return (
 		<nav>
 			<Link to="/"><img src="public/house.svg" alt="homePage" /></Link>
-			<Link to="/cart"><img src="public/basket2.svg" alt="cartPage" /></Link>
+			<div className="">
+				<Link to="/cart"><img src="public/basket2.svg" alt="cartPage" /></Link>
+				<div style={getQuantity() === 0 ? {display: "none"} : {display: "flex"}}>{getQuantity()}</div>
+			</div>
 		</nav>
 	);
 }
